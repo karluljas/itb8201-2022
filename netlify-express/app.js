fbde1f0617df38d8a7c75056ed44643ab99e74e9
karluljas <92479285+karluljas@users.noreply.github.com>
2022-09-13 18:03:19 +0300

    1

---
+++ netlify-express/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+// kuulab päringuid
+app.listen(3000);
+
+//esileht
+app.get("/", (req, res) => {
+   res.sendFile("./dist/index.html", {root: __dirname});
+});
+
+//kuidas esimene kodutöö edenes
+app.get("/about", (req, res) => {
+    res.sendFile("./dist/about.html", {root: __dirname});
+});
+
+//404 leht
+app.use((req, res) => {
+    res.status(404).sendFile("./dist/404.html", {root: __dirname});
+});
